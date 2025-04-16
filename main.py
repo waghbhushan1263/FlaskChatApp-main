dc8e15d3e95ee64eb30bd38b259a118fd9899957
waghbhushan1263 <136261341+waghbhushan1263@users.noreply.github.com>
2025-04-17 01:21:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -242,4 +242,5 @@ if __name__ == '__main__':
     with app.app_context():
        db.create_all()
 
-    socketio.run(app, debug=True, port=8200)
+    port = int(os.environ.get('PORT', 5000))  # Set Render's default port
+    socketio.run(app, host='0.0.0.0', port=port)
